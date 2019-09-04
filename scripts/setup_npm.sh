c55f2390f1295ec438feb2de222ebfb530396d90
Rick Sullivan <rsullivan@focusvision.com>
2019-09-04 14:44:34 -0700

    Ensure `nvm` is on path before NPM is installed

--- scripts/setup_npm.sh
+++ scripts/setup_npm.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+set -ex
+
 if [ ! -x "$(command -v nvm)" ]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  . ~/.bash_profile
 fi
 
 if [ ! -x "$(command -v npm)" ]; then
