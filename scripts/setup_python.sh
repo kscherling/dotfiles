2e7381c5e361dc07fb5f2a2faf1a5ca839d19d16
Rick Sullivan <rsullivan@focusvision.com>
2019-09-04 15:09:33 -0700

    Install pythons 2 and 3

--- scripts/setup_python.sh
+++ scripts/setup_python.sh
@@ -3,10 +3,22 @@
 set -e
 
 if [ ! -x "$(command -v pyenv)" ]; then
-  if [ -x "$(command -v brew)" ]; then
-    brew update
-    brew install pyenv
-  else
+  if [ ! -x "$(command -v brew)" ]; then
     echo "Don't know how to install PyEnv without Homebrew"
+    exit 1
   fi
+
+  brew update
+  brew install pyenv
 fi
+
+if [ ! -x "$(command -v python2)" ]; then
+  pyenv install 2.7.16
+fi
+
+PYTHON3_VERSION="3.7.3"
+if [ ! -x "$(command -v python3)" ]; then
+  pyenv install $PYTHON3_VERSION
+fi
+
+pyenv global $PYTHON3_VERSION
